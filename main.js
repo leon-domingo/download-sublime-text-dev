b47c27c7d5b5d3300f6292b0778725b4dddb5846
León Domingo <leon@codevince.com>
2020-04-18 17:53:37 +0200

    Remove "superagent" dependency and use the core library instead / Some minor fixes and modifications

--- main.js
+++ main.js
@@ -1,45 +1,75 @@
 'use strict';
 
-const Nightmare = require('nightmare'),
-      fs        = require('fs'),
-      request   = require('superagent');
+const Nightmare = require('nightmare')
+const fs        = require('fs')
+const http      = require('https')
 
-const nightmare = Nightmare({ show: false });
-const FILE_NAME = './latest-version';
+// const nightmare = Nightmare({ show: false })
 
-nightmare
-  .goto('https://www.sublimetext.com/3dev')
-  .wait('#dl_linux_64')
+const LAST_VERSION_FILENAME = './.latest-version';
+const SUBLIME_URL           = 'https://www.sublimetext.com/3dev'
+
+Nightmare({ show: false })
+  .goto(SUBLIME_URL)
+  .wait('#dl_linux')
   .evaluate(() => {
-    return document.querySelector('#dl_linux_64 a:nth-child(2)').href
+    return document.querySelector('#direct-downloads li:nth-child(4) a').href
   })
   .end()
   .then(href => {
-    let version_actual = parseInt(/build_(\d+)/.exec(href)[1], 10);
-    fs.readFile(FILE_NAME, (err, data) => {
-      let version_anterior = 0;
+    const versionActual = parseInt(/build_(\d+)/.exec(href)[1], 10)
+
+    fs.readFile(LAST_VERSION_FILENAME, (err, data) => {
+
+      let versionAnterior = 0
       if (!err) {
-        version_anterior = parseInt(data, 10);
+        versionAnterior = parseInt(data.toString(), 10)
       }
 
       // comparar versiones
-      if (version_anterior < version_actual) {
-        console.log(`There is a new version ${version_actual} available: ${href}`);
+      if (versionAnterior < versionActual) {
+        console.log(`There is a new version ${versionActual} available!`);
 
-        fs.writeFile(FILE_NAME, version_actual, err => {
+        fs.writeFile(LAST_VERSION_FILENAME, versionActual, err => {
           if (err) {
-            console.log(err);
+            console.error(err)
           }
-        });
+        })
 
         // https://download.sublimetext.com/sublime_text_3_build_3109_x64.tar.bz2
-        console.log('Downloading...')
-        let file_name = /\/([^\/]+)$/.exec(href)[1];
-        let out = fs.createWriteStream('./' + file_name);
-        request.get(href).pipe(out);
+        const fileName = /\/([^\/]+)$/.exec(href)[1]
+        const downloadStream = fs.createWriteStream('./' + fileName)
+        console.info(`Downloading "${fileName}" (version ${versionActual})...`)
+
+        let percentage = 0
+        let progress = 0
+        http.get(href, res => {
+          // number of bytes to be downloaded
+          const contentLength = parseInt(res.headers['content-length'], 10)
+
+          res.pipe(downloadStream)
+
+          res.on('data', chunk => {
+            const chunkSize = Buffer.byteLength(chunk)
+            progress += chunkSize
+
+            const currentPercentage = Math.trunc((progress / contentLength) * 100)
+            if (percentage !== currentPercentage) {
+              if (percentage % 4 === 0) {
+                process.stdout.write('#')
+              }
+
+              percentage = currentPercentage
+            }
+
+          }).on('end', () => {
+            console.info(`\n"${fileName}" successfully downloaded!`)
+          }).on('error', err => console.error(err))
+        })
       }
       else {
-        console.log(`There's no a newer version than ${version_anterior}`);
+        console.warn(`There's no newer version than ${versionAnterior}`)
+        process.exit(1)
       }
-    });
-  });
+    })
+  })
